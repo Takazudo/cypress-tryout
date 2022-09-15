d03941307bce6e003eb5e433435fbe82df3c4994
Takeshi Takatsudo <takazudo@gmail.com>
2022-09-15 18:44:51 +0900

    updated next.js apps

---
+++ pages/other-page.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Other Page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Other Page.</main>
+    </div>
+  );
+}
